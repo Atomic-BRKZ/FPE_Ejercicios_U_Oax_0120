71ea53ce0a37dfb617ecb309f231ee6d0cedc085
Atomic-BRKZ <61020669+Atomic-BRKZ@users.noreply.github.com>
2020-04-23 19:11:13 -0500

    Update CalculoDeVolumenes.c

--- CalculoDeVolumenes.c
+++ CalculoDeVolumenes.c
@@ -1,36 +1,50 @@
-//Programa que llama a diferentes funciones para calcular el volumen de cuerpos geométricos.
-
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+float VParale(float x,float y,float t);
+float Vcili(float t,float v);
+ int main()
+ {
+ 	float a,l,h,r,h1;
+ 	char rpt;
+ 	printf("Programa que calcula el volumen de figuras geometricas \n");
+ 		printf("Deseas calcular el area de un Paralelepipedo o de un Cilindro? p/c\tt\n");
+ 		scanf("%c",&rpt);
+ 		switch(rpt)
+ 		{
+ 			case 'p':
+ 		 	do{
+ 		 		printf("Dame el primer lado\n");
+ 		 		 scanf("%f",&a);
+ 		 		printf("Dame el segundo lado\n");
+ 		 		 scanf("%f",&l);
+ 		 		printf("Dame la altura\n");
+ 		 		 scanf("%f",&h);
+ 		 		 }while((a==0)||(l==0)||(h==0));
+ 		 		 printf("El volumen del paralelepipedo es de %f \n",VParale(a,l,h));
+ 		 		 break;
+	     case 'c':
+	     	 do{
+	     	 	printf("Dame el Radio del cilindro");
+ 		 		 scanf("%f",&r);
+ 		 		printf("Dame la altura");
+ 		 		 scanf("%f",&h1);
+			  }while((r==0)||(h1==0));
+			  printf("El volumen del paralelepipedo es de %f \n",Vcili(l,h1));
+ 	           break;
+		 }
+ 		 
+ 	 return 0;
+ }
+ 
+ float VParale(float x,float y,float t)
+    {
+    	float volp=x*y*t;
+    	return volp;
+	}
 
-
-float volumen_cono(float radio, float altura);
-float volumenDeEsfera(float radio);
-
-int main()
-{
-    char c;
-    float radio_base_cono, altura_cono, radio_esfera;
-    printf("Programa que calcula el volumen de cuerpos geometricos\n");
-    /*printf("Calculo del volumen de un cono\n");
-    printf("Ingrese el valor del radio de la base del cono: ");
-    scanf("%f", &radio_base_cono);
-    while ((c = getc(stdin)) != '\n' && c != EOF); //Esta linea hace flush a la entrada
-    printf("Ingrese el valor de la altura del cono: ");
-    scanf("%f", &altura_cono);
-    while ((c = getc(stdin)) != '\n' && c != EOF); //Esta linea hace flush a la entrada
-    printf("El volumen del prisma es: %f", volumen_cono(radio_base_cono, altura_cono));*/
-
-    printf("\n");
-    printf("db    db  .d88b.  db      db    db .88b  d88. d88888b d8b   db \n");
-    printf("88    88 .8P  Y8. 88      88    88 88'YbdP`88 88'     888o  88 \n");
-    printf("Y8    8P 88    88 88      88    88 88  88  88 88ooooo 88V8o 88 \n");
-    printf("`8b  d8' 88    88 88      88    88 88  88  88 88~~~~~ 88 V8o88 \n");
-    printf(" `8bd8'  `8b  d8' 88booo. 88b  d88 88  88  88 88.     88  V888 \n");
-    printf("   YP     `Y88P'  Y88888P ~Y8888P' YP  YP  YP Y88888P VP   V8P \n");
-    printf("\n");
-    printf("Radio de la esfera:\n");
-    scanf("%f", &radio_esfera);
-    while ((c = getc(stdin)) != '\n' && c != EOF); //Esta linea hace flush a la entrada
-    printf("Volumen dela esfera es: %f",volumenDeEsfera(radio_esfera));
-}
+float Vcili(float t,float v)
+    {
+    	float volc=t*v*M_PI;
+    	return volc;
+	}
