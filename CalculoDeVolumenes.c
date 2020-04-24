b05caf58a205a6c9482e23d949af58119f52b198
Atomic-BRKZ <61020669+Atomic-BRKZ@users.noreply.github.com>
2020-04-24 11:48:25 -0500

    Update CalculoDeVolumenes.c
    Cambios del equipo 2

--- CalculoDeVolumenes.c
+++ CalculoDeVolumenes.c
@@ -6,10 +6,13 @@
 
 float volumen_cono(float radio, float altura);
 float volumenDeEsfera(float radio);
+float VParale(float x,float y,float t);
+float Vcili(float t,float v);
 
 int main()
 {
-    char c;
+    float a,l,h,r,h1;
+    char c,rpt;
     float radio_base_cono, altura_cono, radio_esfera, altura_prisma, area_base_prisma;
     printf("Programa que calcula el volumen de cuerpos geometricos\n");
     printf("\n");
@@ -43,4 +46,30 @@ int main()
     while ((c = getc(stdin)) != '\n' && c != EOF); //Esta linea hace flush a la entrada
     printf("El volumen del cono es %.4lf u^3", volumen_cono(radio_base_cono,altura_cono));
     
+    printf("Programa que calcula el volumen de figuras geometricas \n");
+ 		printf("Deseas calcular el area de un Paralelepipedo o de un Cilindro? p/c\tt\n");
+ 		scanf("%c",&rpt);
+ 		switch(rpt)
+ 		{
+ 			case 'p':
+ 		 	do{
+ 		 		printf("Dame el primer lado\n");
+ 		 		 scanf("%f",&a);
+ 		 		printf("Dame el segundo lado\n");
+ 		 		 scanf("%f",&l);
+ 		 		printf("Dame la altura\n");
+ 		 		 scanf("%f",&h);
+ 		 		 }while((a==0)||(l==0)||(h==0));
+ 		 		 printf("El volumen del paralelepipedo es de %f \n",VParale(a,l,h));
+ 		 		 break;
+	     case 'c':
+	     	 do{
+	     	 	printf("Dame el Radio del cilindro");
+ 		 		 scanf("%f",&r);
+ 		 		printf("Dame la altura");
+ 		 		 scanf("%f",&h1);
+			  }while((r==0)||(h1==0));
+			  printf("El volumen del paralelepipedo es de %f \n",Vcili(l,h1));
+ 	           break;
+		 }
 }
